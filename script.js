c91130eafe375c885a95ebd702ca669577ab967a
Carlos Eduardo Pegoraro Lopes <carlospegorarolopes@gmail.com>
2022-05-28 03:11:07 -0300

    modification in index.html

--- script.js
+++ script.js
@@ -1,52 +1,15 @@
-let images = document.querySelectorAll('.images img');
-let mainImage = document.querySelectorAll('.imagens .item');
-let prev = document.querySelector('.control-prev');
-let next = document.querySelector('.control-next');
-let close = document.querySelector('#close');
-let galleryImg = document.querySelector('.gallery-inner img');
-let gallery = document.querySelector('.gallery')
-let currentIndex = 0;
-function showGallery(){
-    gallery.classList.add('active');
-    galleryImg.src = images[currentIndex].src;
-}
-images.forEach((item,index)=>{
-    item.addEventListener('click', function(){
-        currentIndex = index;
-        showGallery()
-    })
-})
-
-close.addEventListener('click',function(){
-    gallery.classList.remove('active');
-})
-document.addEventListener('keydown', function(e){
-    if(e.keyCode == 27){ // nút esc sẽ xoá 
-        gallery.classList.remove('active');
+//main
+document.querySelector("#imagesMain")
+.addEventListener("wheel", event => {
+    if(event.deltaY > 0) {
+        event.target.scrollBy(400, 0)
+    } else {
+        event.target.scrollBy(-400, 0)
     }
 })
-prev.addEventListener('click',function(){
-   if(currentIndex > 0){
-    currentIndex--;
-    showGallery()
-   }
-   else{
-    currentIndex = images.length-1;
-    showGallery()
-   }
-})
-next.addEventListener('click',function(){
-    max = images.length - 1;
-    if(currentIndex >= max){
-        currentIndex = -1;
-    }
-    currentIndex++;
-    showGallery()
-})
 
-//Carrossel main
-
-document.querySelector("#imagens")
+//footer
+document.querySelector("#images")
 .addEventListener("wheel", event => {
     if(event.deltaY > 0) {
         event.target.scrollBy(400, 0)
